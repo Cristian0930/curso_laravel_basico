8042ca904d7f87afdca3083da5c5c2dec8d03e4d
Cristian López Ramirez <cl827209@gmail.com>
2022-02-22 14:15:16 -0600

    validación de datos con FormRequest

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Form Request</h1>
+        <form action="{{ route('posts.store') }}" method="POST">
+            @csrf
+            <input type="text" name="title" />
+            <input type="submit" value="Enviar" />
+        </form>
+    </body>
+</html>
