8aa4ddb9879071d6042c970ec8ef5c73c2edb64f
Cristian López Ramirez <cl827209@gmail.com>
2022-02-22 19:20:44 -0600

    Eloquent: trabajando con este ORM

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('eloquent', function () {
+    $posts = Post::where('id', '>=', '20')
+        ->orderBy('id', 'desc')
+        ->take(3)
+        ->get();
+
+    foreach ($posts as $post) {
+        echo "$post->id - $post->title <br>";
+    }
 });
